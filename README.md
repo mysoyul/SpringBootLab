bc83e32b99f9e461d1b7ba8bb64bfd6ca90243f9
mysoyul <vega2k_@naver.com>
2025-05-12 08:18:12 +0900

    bookdetail add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Lab 2-3 book service
 Lab 2-4 수업중 student 와 student_detail 1 대 1 service
+Lab 2-4 book 과 book_detail 1 대 1 service
--- src/main/java/com/rookies3/myspringbootlab/entity/Book.java
+++ src/main/java/com/rookies3/myspringbootlab/entity/Book.java
@@ -1,26 +1,33 @@
 package com.rookies3.myspringbootlab.entity;
 
+import com.rookies3.myspringbootlab.entity.BookDetail;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "books")
-@Getter @Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "book_id")
     private Long id;
-    
+
     @Column(nullable = false)
     private String title;
-    
+
     @Column(nullable = false)
     private String author;
-    
+
     @Column(unique = true, nullable = false)
     private String isbn;
 
@@ -28,4 +35,6 @@ public class Book {
 
     private LocalDate publishDate;
 
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private BookDetail bookDetail;
 }
