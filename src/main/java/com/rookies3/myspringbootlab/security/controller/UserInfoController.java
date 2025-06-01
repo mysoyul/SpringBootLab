2ae6dc0a01ab2870cc591e16715277dea26e1988
mysoyul <vega2k_@naver.com>
2025-06-01 16:44:49 +0900

    Security Config add

---
+++ src/main/java/com/rookies3/myspringbootlab/security/controller/UserInfoController.java
@@ -0,0 +1,16 @@
+package com.rookies3.myspringbootlab.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserInfoController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+}
